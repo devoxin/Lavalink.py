9b94b6a2dccfde63a3051368e77a2f86bcb182aa
Devoxin <lukel9914@gmail.com>
2017-12-12 19:19:09 +0000

    Bump setup.py version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='Lavalink.py',
-    version='1.0.1',
+    version='1.0.2',
     packages=['websockets', 'aiohttp', 'asyncio'],
     url='https://github.com/Devoxin/Lavalink.py',
     license='MIT',
     author='Luke, William',
     author_email='atiwiex@gmail.com',
-    description='Lavalink wrapper '
+    description='A Lavalink interface for Python'
 )
