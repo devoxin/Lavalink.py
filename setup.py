9a214a9abe138b9f5c68aede77804b6766a8d2c5
William <atiwiex@users.noreply.github.com>
2017-12-12 08:21:49 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='Lavalink.py',
-    version='1.0.1',
-    packages=['websockets', 'aiohttp'],
-    url='https://github.com/Devoxin/Lavalink.py',
-    license='MIT',
-    author='Luke, William',
-    author_email='atiwiex@gmail.com',
-    description='Lavalink wrapper '
+  name='lavalink',
+  packages=['lavalink'],
+  version='1.0.1',
+  description='Lavalink python wrapper',
+  author='Luke, William',
+  author_email='atiwiex@gmail.com',
+  url='https://github.com/Devoxin/Lavalink.py',
+  download_url='https://github.com/Devoxin/Lavalink.py/archive/1.0.1.tar.gz',
+  keywords=['lavalink'],
 )
