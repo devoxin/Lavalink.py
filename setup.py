bbb55f2af7a027b75a54344f0c183407fbcba9dd
Devoxin <lukel9914@gmail.com>
2018-02-10 19:18:29 +0000

    ...

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
   name='lavalink',
   packages=['lavalink'],
-  version='2.0.0',
+  version='2.0.1',
   description='A lavalink interface built for discord.py',
   author='Luke, William',
   author_email='dev@crimsonxv.pro',
   url='https://github.com/Devoxin/Lavalink.py',
-  download_url='https://github.com/Devoxin/Lavalink.py/archive/1.0.1.tar.gz',
+  download_url='https://github.com/Devoxin/Lavalink.py/archive/2.0.1.tar.gz',
   keywords=['lavalink'],
 )
