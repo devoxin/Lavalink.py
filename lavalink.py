a5947f7124f87ad78496e5b36649d91f593d657b
Devoxin <lukel9914@gmail.com>
2018-02-10 15:51:52 +0000

    Docstrings, cleanup

--- lavalink.py
+++ lavalink.py
@@ -202,6 +202,7 @@ class WebSocket:
             self.log('warn', 'Unable to reconnect to Lavalink!')
 
     async def send(self, **data):
+        """ Sends data to lavalink """
         if not self._ws or not self._ws:
             self._queue.append(data)
             self.log('verbose', 'Websocket not ready; appending payload to queue\n' + str(data))
@@ -217,6 +218,7 @@ class InvalidTrack(Exception):
 
 class AudioTrack:
     def build(self, track, requester):
+        """ Returns an optional AudioTrack """
         try:
             self.track = track['track']
             self.identifier = track['info']['identifier']
@@ -236,39 +238,49 @@ class AudioTrack:
 class PlayerManager:
     def __init__(self, bot):
         self.bot = bot
-        self.players = {}
+        self._players = {}
 
     def __len__(self):
-        return len(self.players)
+        return len(self._players)
 
     def __getitem__(self, item):
-        return self.players.get(item, None)
+        return self._players.get(item, None)
 
     def __contains__(self, item):
-        return item in self.players
+        return item in self._players
 
     def find(self, predicate):
-        found = list(filter(predicate, self.players))
+        """ Returns the first player in the list based on the given filter predicate. Could be None """
+        found = self.find_all(predicate)
         return found[0] if found else None
 
     def find_all(self, predicate):
-        return list(filter(predicate, self.players))
+        """ Returns a list of players based on the given filter predicate """
+        return list(filter(predicate, self._players))
 
     def get(self, guild_id):
-        if guild_id not in self.players:
+        """ Returns a player from the cache, or creates one if it does not exist """
+        if guild_id not in self._players:
             p = Player(bot=self.bot, guild_id=guild_id)
-            self.players[guild_id] = p
+            self._players[guild_id] = p
 
-        return self.players[guild_id]
+        return self._players[guild_id]
 
-    def has(self, guild_id):
-        return guild_id in self.players
+    def has(self, guild_id) -> bool:
+        """ Returns the presence of a player in the cache """
+        return guild_id in self._players
 
     def clear(self):
-        self.players.clear()
+        """ Removes all of the players from the cache """
+        self._players.clear()
 
     def get_playing(self):
-        return len([p for p in self.players.values() if p.is_playing])
+        """ Returns the amount of players that are currently playing """
+        return len([p for p in self._players.values() if p.is_playing])
+    
+    def use_player(self, player):
+        """ Not implemented """
+        raise NotImplementedError  # :)
 
 
 class Player:
@@ -289,21 +301,25 @@ class Player:
         self.current = None
 
     @property
-    def is_playing(self):
+    def is_playing(self) -> bool:
+        """ Returns the player's track state """
         return self.connected_channel is not None and self.current is not None
 
     @property
-    def is_connected(self):
+    def is_connected(self) -> bool:
+        """ Returns the player's connection state """
         return self.connected_channel is not None
 
     @property
     def connected_channel(self):
+        """ Returns the voicechannel the player is connected to """
         if not self.channel_id:
             return None
 
         return self.bot.get_channel(int(self.channel_id))
     
     async def connect(self, channel):
+        """ Connects to a voicechannel """
         payload = {
             'op': 4,
             'd': {
@@ -317,6 +333,7 @@ class Player:
         self.channel_id = str(channel.id)
 
     async def disconnect(self):
+        """ Disconnects from the voicechannel, if any """
         if not self.is_connected:
             return
         
@@ -336,19 +353,22 @@ class Player:
         self.channel_id = None
 
     def store(self, key, value):
+        """ Stores custom user data """
         self._user_data.update({key: value})
 
     def fetch(self, key, default=None):
+        """ Retrieves the related value from the stored user data """
         return self._user_data.get(key, default)
 
     async def add(self, requester, track, interrupt=False):
-        """ Plays the track if nothing is playing, otherwise adds it to the queue """
+        """ Adds a track to the queue, and plays it immediately if not playing """
         self.queue.append(AudioTrack().build(track, requester))
 
         if not self.is_playing or interrupt:
             await self.play()
 
     async def play(self):
+        """ Plays the first track in the queue, if any """
         if not self.queue:
             return
 
@@ -362,17 +382,21 @@ class Player:
         await self.bot.lavalink.client._trigger_event('TrackStartEvent', self.guild_id)
 
     async def stop(self):
+        """ Stops the player, if playing """
         await self.bot.lavalink.ws.send(op='stop', guildId=self.guild_id)
         self.current = None
 
     async def skip(self):
+        """ Moves the player onto the next track in the queue """
         await self.play()
 
-    async def set_paused(self, pause: bool):
+    async def set_pause(self, pause: bool):
+        """ Sets the player's paused state """
         await self.bot.lavalink.ws.send(op='pause', guildId=self.guild_id, pause=pause)
         self.paused = pause
 
     async def set_volume(self, vol: int):
+        """ Sets the player's volume (150% limit imposed by lavalink) """
         if isinstance(vol, int):
             self.volume = max(min(vol, 150), 0)
 
@@ -380,32 +404,23 @@ class Player:
             return self.volume
 
     async def seek(self, pos: int):
+        """ Seeks to a given position in the track """
         await self.bot.lavalink.ws.send(op='seek', guildId=self.guild_id, position=pos)
 
-    async def on_track_end(self, reason: str):
+    async def on_track_end(self):
         self.position = 0
         self.paused = False
         self.current = None
 
-        if reason == 'FINISHED':
-            await self.play()
+        await self.play()
 
 
 class Utils:
 
     @staticmethod
     def format_time(time):
+        """ Formats the given time into HH:MM:SS """
         seconds = (time / 1000) % 60
         minutes = (time / (1000 * 60)) % 60
         hours = (time / (1000 * 60 * 60)) % 24
         return "%02d:%02d:%02d" % (hours, minutes, seconds)
-
-    @staticmethod
-    def get_number(num, default=1):
-        if num is None:
-            return default
-
-        try:
-            return int(num)
-        except ValueError:
-            return default
