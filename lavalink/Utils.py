e168e0daa7be62cfccd062415c1e2c4a1f7d3b7d
Devoxin <lukel9914@gmail.com>
2018-02-27 18:05:40 +0000

    More reliable time-parsing

--- lavalink/Utils.py
+++ lavalink/Utils.py
@@ -3,7 +3,7 @@ class Utils:
     @staticmethod
     def format_time(time):
         """ Formats the given time into HH:MM:SS """
-        seconds = (time / 1000) % 60
-        minutes = (time / (1000 * 60)) % 60
-        hours = (time / (1000 * 60 * 60)) % 24
-        return "%02d:%02d:%02d" % (hours, minutes, seconds)
+        h, r = divmod(time / 1000, 3600)
+        m, s = divmod(r, 60)
+
+        return "%02d:%02d:%02d" % (h, m, s)
