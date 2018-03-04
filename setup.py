33d647ffa8dfd7eb302967fd8a8f0d962c7aa331
Will Tekulve <tekulve.will@gmail.com>
2018-03-04 01:50:08 -0500

    Add requirements to setup

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from setuptools import setup
 
+def get_requirements():
+    with open('requirements.txt') as f:
+        requirements = f.read().splitlines()
+    return requirements
+
 setup(
-  name='lavalink',
-  packages=['lavalink'],
-  version='2.0.2.9',
-  description='A lavalink interface built for discord.py',
-  author='Luke, William',
-  author_email='dev@crimsonxv.pro',
-  url='https://github.com/Devoxin/Lavalink.py',
-  download_url='https://github.com/Devoxin/Lavalink.py/archive/2.0.2.9.tar.gz',
-  keywords=['lavalink'],
+    name='lavalink',
+    packages=['lavalink'],
+    version='2.0.2.9',
+    description='A lavalink interface built for discord.py',
+    author='Luke, William',
+    author_email='dev@crimsonxv.pro',
+    url='https://github.com/Devoxin/Lavalink.py',
+    download_url='https://github.com/Devoxin/Lavalink.py/archive/2.0.2.9.tar.gz',
+    keywords=['lavalink'],
+    include_package_data=True,
+    install_requires=get_requirements()
 )
