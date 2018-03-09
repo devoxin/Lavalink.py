610c68a4281d7997f4ee3d71ca0e3a093c91770e
Luke <dev@crimsonxv.pro>
2018-03-09 03:35:03 +0000

    Prepare for new release

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ def get_requirements():
 setup(
     name='lavalink',
     packages=['lavalink'],
-    version='2.0.2.9',
+    version='2.1',
     description='A lavalink interface built for discord.py',
     author='Luke, William',
     author_email='dev@crimsonxv.pro',
     url='https://github.com/Devoxin/Lavalink.py',
-    download_url='https://github.com/Devoxin/Lavalink.py/archive/2.0.2.9.tar.gz',
+    download_url='https://github.com/Devoxin/Lavalink.py/archive/2.1.tar.gz',
     keywords=['lavalink'],
     include_package_data=True,
     install_requires=get_requirements()
