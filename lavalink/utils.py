b596867c82a0ba0896354bc4c49fcec7c1ab0962
Will Tekulve <tekulve.will@gmail.com>
2018-03-08 22:43:45 -0500

    Refactor utils

--- lavalink/utils.py
+++ lavalink/utils.py
@@ -1,9 +1,8 @@
-class Utils:
+__all__ = ["format_time",]
 
-    @staticmethod
-    def format_time(time):
-        """ Formats the given time into HH:MM:SS """
-        h, r = divmod(time / 1000, 3600)
-        m, s = divmod(r, 60)
+def format_time(time):
+    """ Formats the given time into HH:MM:SS """
+    h, r = divmod(time / 1000, 3600)
+    m, s = divmod(r, 60)
 
-        return "%02d:%02d:%02d" % (h, m, s)
+    return "%02d:%02d:%02d" % (h, m, s)
