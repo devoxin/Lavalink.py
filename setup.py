4a9c011b04e46e51514936bc8c1b58e919adc97a
Devoxin <lukel9914@gmail.com>
2018-03-18 16:00:51 +0000

    2.1 pypi release was broken

--- setup.py
+++ setup.py
@@ -1,21 +1,15 @@
 from setuptools import setup
 
 
-def get_requirements():
-    with open('requirements.txt') as f:
-        requirements = f.read().splitlines()
-    return requirements
-
-
 setup(
     name='lavalink',
     packages=['lavalink'],
-    version='2.1',
+    version='2.1.1',
     description='A lavalink interface built for discord.py',
     author='Luke, William',
     author_email='dev@crimsonxv.pro',
     url='https://github.com/Devoxin/Lavalink.py',
-    download_url='https://github.com/Devoxin/Lavalink.py/archive/2.1.tar.gz',
+    download_url='https://github.com/Devoxin/Lavalink.py/archive/2.1.1.tar.gz',
     keywords=['lavalink'],
     include_package_data=True,
     install_requires=['websockets>=4.0.0', 'aiohttp']
