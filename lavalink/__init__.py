f30cb1ac2dd5f6c66cef6629ab6bd2e80abb3174
Devoxin <lukel9914@gmail.com>
2018-03-24 15:41:09 +0000

    bump ver

--- lavalink/__init__.py
+++ lavalink/__init__.py
@@ -2,7 +2,7 @@ __title__ = 'Lavalink'
 __author__ = 'Luke & William'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2018 Luke & William'
-__version__ = '2.1.3'
+__version__ = '2.1.4'
 
 from .AudioTrack import *
 from .Client import *
