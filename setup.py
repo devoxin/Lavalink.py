f284749762f5185f74b97f66b2872208c695ec98
Devoxin <lukel9914@gmail.com>
2018-07-18 20:31:35 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from setuptools import setup
 setup(
     name='lavalink',
     packages=['lavalink'],
-    version='2.1.8',
+    version='2.1.9',
     description='A lavalink interface built for discord.py',
     author='Devoxin',
     author_email='luke@serux.pro',
     url='https://github.com/Devoxin/Lavalink.py',
-    download_url='https://github.com/Devoxin/Lavalink.py/archive/2.1.8.tar.gz',
+    download_url='https://github.com/Devoxin/Lavalink.py/archive/2.1.9.tar.gz',
     keywords=['lavalink'],
     include_package_data=True,
     install_requires=['websockets!=5.0.0', 'aiohttp']
