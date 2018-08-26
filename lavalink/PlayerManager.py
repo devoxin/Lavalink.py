1787d0765f0f1c6a0e9f1e5d1586adc86d73968d
AndyTempel <andraz@korenc.eu>
2018-08-26 22:59:06 +0200

    up to 1000 volume

--- lavalink/PlayerManager.py
+++ lavalink/PlayerManager.py
@@ -139,7 +139,7 @@ class DefaultPlayer(BasePlayer):
 
     async def set_volume(self, vol: int):
         """ Sets the player's volume (150% limit imposed by lavalink) """
-        self.volume = max(min(vol, 150), 0)
+        self.volume = max(min(vol, 1000), 0)
         await self._lavalink.ws.send(op='volume', guildId=self.guild_id, volume=self.volume)
 
     async def seek(self, pos: int):
