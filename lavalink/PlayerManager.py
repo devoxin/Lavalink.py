932264a03922f0048c4468b89acee477f78e2b28
AndyTempel <andraz@korenc.eu>
2018-10-02 21:01:11 +0200

    Fixing weird indent

--- lavalink/PlayerManager.py
+++ lavalink/PlayerManager.py
@@ -169,8 +169,8 @@ class DefaultPlayer(BasePlayer):
 
     async def handle_event(self, event):
         """ Makes the player play the next song from the queue if a song has finished or an issue occurred. """
-        if isinstance(event, (TrackStuckEvent, TrackExceptionEvent)) or isinstance(event,
-                                                                                   TrackEndEvent) and event.reason == 'FINISHED':
+        if isinstance(event, (TrackStuckEvent, TrackExceptionEvent)) or \
+                isinstance(event, TrackEndEvent) and event.reason == 'FINISHED':
             await self.play()
 
 
