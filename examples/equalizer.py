45fb8c30d0e1e8e4137bfedf43820b5d777f82b4
Luke <15076404+Devoxin@users.noreply.github.com>
2018-10-28 01:11:34 +0100

    Equalizer now shows band frequencies

--- examples/equalizer.py
+++ examples/equalizer.py
@@ -5,6 +5,9 @@ class Equalizer:
     def __init__(self):
         self._band_count = 15
         self.bands = [0.0 for x in range(self._band_count)]
+        self.freqs = ['25', '40', '63', '100', '160', '250',
+                      '400', '630', '1K', '1.6', '2.5', '4K',
+                      '6.3', '10K', '16K']
 
     def set_gain(self, band: int, gain: float):
         if band < 0 or band >= self._band_count:
@@ -22,7 +25,7 @@ class Equalizer:
 
     def visualise(self):
         block = ''
-        bands = [str(band).zfill(2) for band in range(self._band_count)]
+        bands = [f'{self.freqs[band]:>3}' for band in range(self._band_count)]
         bottom = (' ' * 8) + '  '.join(bands)
 
         gains = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0.0, -0.1, -0.2, -0.25]
@@ -41,11 +44,11 @@ class Equalizer:
 
             for value in self.bands:
                 if value >= gain:
-                    block += '▄▄  '
+                    block += '▄▄▄  '
                 else:
-                    block += '    '
+                    block += '     '
 
             block += '\n'
 
         block += bottom
-        return block
+        return block.strip()
