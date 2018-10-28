645565e4cd3bb654b38c325417bb72cd6dc0f9ad
Luke <15076404+Devoxin@users.noreply.github.com>
2018-10-28 13:29:14 +0000

    vers bump in init

--- lavalink/__init__.py
+++ lavalink/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'Lavalink'
 __author__ = 'Devoxin'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2018 Devoxin'
-__version__ = '2.1.10'
+__version__ = '3.0.0'
 
 import logging
 import sys
