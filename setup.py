f08f33c47e8aebbabdfce80a158c1d058e5c4209
Devoxin <lukel9914@gmail.com>
2018-10-28 13:36:48 +0000

    Auto-pull version from __init__

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
+import re
+
 from setuptools import setup
 
+
+version = ''
+with open('lavalink/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if not version:
+    raise RuntimeError('version is not set')
 
 setup(
     name='lavalink',
     packages=['lavalink'],
-    version='3.0.0',
+    version=version,
     description='A lavalink interface built for discord.py',
     author='Devoxin',
     author_email='luke@serux.pro',
     url='https://github.com/Devoxin/Lavalink.py',
-    # download_url='https://github.com/Devoxin/Lavalink.py/archive/3.0.0.tar.gz',
+    download_url='https://github.com/Devoxin/Lavalink.py/archive/{}.tar.gz'.format(version),
     keywords=['lavalink'],
     include_package_data=True,
     install_requires=['websockets>=5.0.1,<6.1.0', 'aiohttp']
