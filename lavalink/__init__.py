26a40896046fad4df977cbaddb263f73eb68734e
Devoxin <lukel9914@gmail.com>
2018-11-06 21:26:50 +0000

    lit

--- lavalink/__init__.py
+++ lavalink/__init__.py
@@ -5,28 +5,3 @@ __author__ = 'Devoxin'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2018 Devoxin'
 __version__ = '3.0.0'
-
-import logging
-import sys
-
-from .AudioTrack import *
-from .Client import *
-from .PlayerManager import *
-from .WebSocket import *
-from . import Events
-from . import Utils
-from .NodeManager import Regions
-
-
-log = logging.getLogger(__name__)
-
-fmt = logging.Formatter(
-    '[%(asctime)s] [lavalink.py] [%(levelname)s] %(message)s',
-    datefmt="%H:%M:%S"
-)
-
-handler = logging.StreamHandler(sys.stdout)
-handler.setFormatter(fmt)
-log.addHandler(handler)
-
-log.setLevel(logging.INFO)
