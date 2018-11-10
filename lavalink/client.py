f0ee7fd2e3fdca135233d4d1174edbdb91f2bced
Devoxin <lukel9914@gmail.com>
2018-11-10 22:56:08 +0000

    Voice update handling

--- lavalink/client.py
+++ lavalink/client.py
@@ -5,6 +5,7 @@ import random
 from urllib.parse import quote
 from .node import Node
 from .nodemanager import NodeManager
+from .playermanager import PlayerManager
 
 log = logging.getLogger(__name__)
 
@@ -14,7 +15,8 @@ class Client:
         self._user_id = str(user_id)
         self._shard_count = str(shard_count)
         self._loop = loop or asyncio.get_event_loop()
-        self._node_manager = NodeManager()
+        self.node_manager = NodeManager()
+        self.players = PlayerManager()
 
         self._session = aiohttp.ClientSession(
             connector=aiohttp.TCPConnector(limit=pool_size, loop=loop)
@@ -29,7 +31,7 @@ class Client:
         :param node:
             The node to use for track lookup. Leave this blank to use a random node.
         """
-        node = node or random.choice(self._node_manager.nodes)
+        node = node or random.choice(self.node_manager.nodes)
         destination = 'http://{}:{}/loadtracks?identifier={}'.format(node.host, node.port, quote(query))
         headers = {
             'Authorization': node.password
@@ -40,3 +42,36 @@ class Client:
                 return await res.json()
 
             return []
+
+    async def voice_update_handler(self, data):
+        """
+        This function intercepts websocket data from your Discord library and
+        forwards the relevant information on to Lavalink, which is used to
+        establish a websocket connection and send audio packets to Discord.
+
+        Example usage for Discord.py:
+        -------------
+        bot.add_listener(lavalink_client, 'on_socket_response')
+        -------------
+        :param data:
+            The payload received from Discord.
+        """
+        if not data or 't' not in data:
+            return
+
+        if data['t'] == 'VOICE_SERVER_UPDATE':
+            guild_id = int(data['d']['guild_id'])
+            player = self.players.get(guild_id, create=False)
+
+            if player:
+                player.node._voice_server_update(data['d'])
+        elif data['t'] == 'VOICE_STATE_UPDATE':
+            if int(data['d']['user_id']) != int(self._user_id):
+                return
+
+            player = self.players.get(guild_id, create=False)
+
+            if player:
+                player.node._voice_state_update(data['d'])
+        else:
+            return
