4a94be3551161ee1501b66d3d17355b91be11fe4
Devoxin <lukel9914@gmail.com>
2021-02-19 19:19:54 +0000

    Specify decode_errors when decoding author too

--- lavalink/utils.py
+++ lavalink/utils.py
@@ -85,10 +85,10 @@ def decode_track(track, decode_errors='ignore'):
     reader = DataReader(track)
 
     flags = (reader.read_int() & 0xC0000000) >> 30
-    version, = struct.unpack('B', reader.read_byte()) if flags & 1 != 0 else 1  # pylint: disable=unused-variable
+    version = struct.unpack('B', reader.read_byte()) if flags & 1 != 0 else 1  # pylint: disable=unused-variable
 
     title = reader.read_utf().decode(errors=decode_errors)
-    author = reader.read_utf().decode()
+    author = reader.read_utf().decode(errors=decode_errors)
     length = reader.read_long()
     identifier = reader.read_utf().decode()
     is_stream = reader.read_boolean()
