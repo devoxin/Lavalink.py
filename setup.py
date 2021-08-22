65de576fb2885e4c5746e928539245b9cbea4325
SilentJungle399 <75272148+SilentJungle399@users.noreply.github.com>
2021-08-22 18:59:47 +0530

    was creating the ms cpp build tools error

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     download_url='https://github.com/Devoxin/Lavalink.py/archive/{}.tar.gz'.format(version),
     keywords=['lavalink'],
     include_package_data=True,
-    install_requires=['aiohttp>=3.6.0,<3.7.0'],
+    install_requires=['aiohttp>=3.6.0'],
     extras_require={'docs': ['sphinx',
                              'pygments',
                              'guzzle_sphinx_theme'],
