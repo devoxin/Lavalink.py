40be69d3cac59388e76274fb83c992318d3d7236
Devoxin <lukel9914@gmail.com>
2021-12-19 15:21:13 +0000

    Testing RTD fixes

--- docs/conf.py
+++ docs/conf.py
@@ -89,6 +89,6 @@ html_show_sourcelink = False
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-html_context = {
-    'css_files': ['_static/style.css']
-}
+# html_context = {
+#     'css_files': ['_static/style.css']
+# }
