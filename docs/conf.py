33645c9f2205a96c696fb45ea933d3a9fa468a4e
Devoxin <lukel9914@gmail.com>
2021-12-19 14:58:46 +0000

    Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 
 project = 'Lavalink.py'
-copyright = '2020, Devoxin'
+copyright = '2021, Devoxin'
 author = 'Devoxin'
 master_doc = 'index'
 
