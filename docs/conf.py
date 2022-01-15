272dec149b51f95a8b56b7bc5265e1b8ff711ceb
Devoxin <lukel9914@gmail.com>
2022-01-15 20:17:05 +0000

    Enlarge deprecation warning

--- docs/conf.py
+++ docs/conf.py
@@ -88,6 +88,7 @@ html_show_sourcelink = False
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = ['style.css']
 
 # html_context = {
 #     'css_files': ['_static/style.css']
