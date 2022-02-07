31f48860c4394318544ba1888933ad614e1d29d4
Devoxin <lukel9914@gmail.com>
2022-02-07 20:25:36 +0000

    missing ,

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     install_requires=['aiohttp>=3.7.4,<3.9.0'],
     extras_require={'docs': ['sphinx',
                              'pygments',
-                             'guzzle_sphinx_theme'
+                             'guzzle_sphinx_theme',
                              'enum_tools',
                              'sphinx_toolbox'],
                     'development': ['pylint',
