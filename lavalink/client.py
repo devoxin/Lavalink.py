840560fed21ec41c202de198989bf6e2b456a711
Rob Wainwright <47543882+apex2504@users.noreply.github.com>
2022-05-26 22:26:23 +0100

    POST is not GET

--- lavalink/client.py
+++ lavalink/client.py
@@ -326,7 +326,7 @@ class Client:
         :class:`bool`
             True if all failing addresses were freed, False otherwise.
         """
-        return await self._post_request('{}/routeplanner/free/all'.format(node.http_uri),
+        return await self._get_request('{}/routeplanner/free/all'.format(node.http_uri),
                                         headers={'Authorization': node.password})
 
     async def get_node_plugins(self, node: Node) -> List[Plugin]:
