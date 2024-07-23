adb9821b49de345c0b7cf14e5500ea67166af5af
devoxin <lukel9914@gmail.com>
2024-07-23 13:26:16 +0100

    5.8.0

--- lavalink/__init__.py
+++ lavalink/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'Lavalink'
 __author__ = 'Devoxin'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017-present Devoxin'
-__version__ = '5.7.0'
+__version__ = '5.8.0'
 
 
 from typing import Type
