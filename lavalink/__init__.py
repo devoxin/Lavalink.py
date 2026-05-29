e7e73f9de05f7568171be4aad3ac61461f94c71e
Devoxin <lukel9914@gmail.com>
2026-05-29 23:13:42 +0100

    Fix versioning, bump to rc

--- lavalink/__init__.py
+++ lavalink/__init__.py
@@ -4,7 +4,7 @@ __title__ = 'Lavalink'
 __author__ = 'Devoxin'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2017-present Devoxin'
-__version__ = '5.11.1a'
+__version__ = '5.11.1rc1'
 
 
 from typing import Type
